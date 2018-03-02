db8b424d863a53cdb3406234a04f224aa80bb1c6
Thoroe <ncnthoroe@gmail.com>
2018-03-02 14:13:26 +0100

    arbejdet i homecontroller og index.html
    Lavet index metode i homecontroller og sat skelet op i index.html

--- src/main/java/dk/adventurealley/app/Controllers/HomeController.java
+++ src/main/java/dk/adventurealley/app/Controllers/HomeController.java
@@ -1,8 +1,15 @@
 package dk.adventurealley.app.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 
 }
