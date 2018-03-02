6013294372aad4a6ac7573674cf501b483fb70f9
Unknown <rednex702@hotmail.com>
2018-03-02 13:16:27 +0100

    opsat CreateAtivitycontroller + viewsPage

---
+++ src/main/java/dk/adventurealley/app/Controllers/CreateActivityController.java
@@ -0,0 +1,17 @@
+package dk.adventurealley.app.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CreateActivityController {
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String createActivity(Model model){
+
+    return "createActivity";
+  }
+
+}
