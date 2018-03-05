fec1319d34e7f1586af7302fc3a7ba0f74c0f0b7
Thoroe <ncnthoroe@gmail.com>
2018-03-05 11:12:38 +0100

    færdiggjort index
    færdiggjort grund arbejdet til index.html

--- src/main/java/dk/adventurealley/app/Controllers/HomeController.java
+++ src/main/java/dk/adventurealley/app/Controllers/HomeController.java
@@ -1,14 +1,20 @@
 package dk.adventurealley.app.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
 
+    //ArrayList<Activity> activities = activityRepo.getActivityList();
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("activities", activities);
         return "index";
     }
 
