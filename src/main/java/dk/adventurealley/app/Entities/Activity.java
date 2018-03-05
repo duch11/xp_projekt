9b794c9c685c6b4acc29dd217a1819470b5adcb2
Unknown <ckflintrup@gmail.com>
2018-03-05 14:01:34 +0100

    2 classer til entities

---
+++ src/main/java/dk/adventurealley/app/Entities/Activity.java
@@ -0,0 +1,77 @@
+package dk.adventurealley.app.Entities;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Ejer on 05-03-2018.
+ */
+public class Activity {
+    private String name;
+    private ArrayList<Requirements> regList;
+    private String equipment;
+    private String imagePath;
+    private String description;
+
+    public Activity(String name, ArrayList<Requirements> regList, String equipment, String imagePath, String description) {
+        this.name = name;
+        this.regList = regList;
+        this.equipment = equipment;
+        this.imagePath = imagePath;
+        this.description = description;
+    }
+
+    public Activity() {
+    }
+
+    @Override
+    public String toString() {
+        return "Activity{" +
+                "name='" + name + '\'' +
+                ", regList=" + regList +
+                ", equipment='" + equipment + '\'' +
+                ", imagePath='" + imagePath + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Requirements> getRegList() {
+        return regList;
+    }
+
+    public void setRegList(ArrayList<Requirements> regList) {
+        this.regList = regList;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
