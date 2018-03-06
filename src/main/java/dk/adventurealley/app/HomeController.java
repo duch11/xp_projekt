900adc9e9b0e8717a3dfd536849d94b2999686ac
Unknown <salikvilmer@gmail.com>
2018-03-06 12:31:28 +0100

    Styling næsten færdig

--- src/main/java/dk/adventurealley/app/HomeController.java
+++ src/main/java/dk/adventurealley/app/HomeController.java
@@ -15,16 +15,19 @@ public class HomeController {
 
     @GetMapping ("/editActivity")
     public String editActivity(Model model){
-        Requirement s1 = new Requirement("højde", "200");
-        Requirement s2 = new Requirement("højd", "2");
-        Requirement s3= new Requirement("høj", "257");
-        Requirement s4 = new Requirement("hø", "202350");
-        Requirement s5 = new Requirement("h", "2023450");
-        requirements.add(s1);
-        requirements.add(s2);
-        requirements.add(s3);
-        requirements.add(s4);
-        requirements.add(s5);
+        if(requirements.isEmpty()) {
+            Requirement s1 = new Requirement("højde", "200");
+            Requirement s2 = new Requirement("højd", "2");
+            Requirement s3 = new Requirement("høj", "257");
+            Requirement s4 = new Requirement("hø", "202350");
+            Requirement s5 = new Requirement("h", "2023450");
+            requirements.add(s1);
+            requirements.add(s2);
+            requirements.add(s3);
+            requirements.add(s4);
+            requirements.add(s5);
+        }
+        model.addAttribute("requirement", new Requirement());
         Activity activity = new Activity("Gokart", requirements, "Hjelm", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFqnVyooVfj3rVcljhdOQSRgcxrowe0GUQJli12phkLzTuh5nL", "haleluja");
         model.addAttribute("requirements", activity.getRequirements());
         model.addAttribute("activity", activity);
