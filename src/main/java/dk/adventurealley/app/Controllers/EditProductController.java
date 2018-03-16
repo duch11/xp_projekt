efa9f4f62891502ca634eb796bff272edf270671
Unknown <davi0279@stud.kea.dk>
2018-03-16 13:21:03 +0100

    Funktionalitet færdig - navigation tilføjet.

--- src/main/java/dk/adventurealley/app/Controllers/EditProductController.java
+++ src/main/java/dk/adventurealley/app/Controllers/EditProductController.java
@@ -1,6 +1,7 @@
 package dk.adventurealley.app.Controllers;
 
 import dk.adventurealley.app.DAO.ProductCreateRepository;
+import dk.adventurealley.app.DAO.ProductRepository;
 import dk.adventurealley.app.Model.Entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,11 +11,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 @Controller
 public class EditProductController {
 
     @Autowired
     ProductCreateRepository proRepo = new ProductCreateRepository();
+
+    @Autowired
+    ProductRepository pr = new ProductRepository();
+
+    ArrayList<Product> productList = new ArrayList<>();
 
     @GetMapping("/editProduct")
     public String editProduct (@RequestParam("id") int id, Model model) {
@@ -25,6 +33,9 @@ public class EditProductController {
     @PostMapping("productEdit")
     public String productEdit (@ModelAttribute Product p, Model model) {
         proRepo.updateProduct(p);
-        return "redirect:/";
+        productList = pr.readAll();
+        model.addAttribute("productList", productList);
+        model.addAttribute("isProductsPage", true);
+        return "viewProducts";
     }
 }
