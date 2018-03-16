27f7485fe5d9c7eabcb84d7ef1e13e015e171c83
Unknown <salikvilmer@gmail.com>
2018-03-16 11:24:16 +0100

    Push for at merge testing

---
+++ src/main/java/dk/adventurealley/app/Controllers/DailyIncomeController.java
@@ -0,0 +1,29 @@
+package dk.adventurealley.app.Controllers;
+
+import dk.adventurealley.app.DAO.DailyIncomeRepository;
+import dk.adventurealley.app.Model.Entities.Sale;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+
+@Controller
+public class DailyIncomeController {
+
+    private DailyIncomeRepository dIR;
+    @GetMapping("/dailyincome")
+    public String dailyIncome(Model model){
+
+
+        model.addAttribute("dailyIncomeArray", dIR.readAll());
+        return "viewTotalDailyIncome";
+    }
+
+
+
+}
+
+
---
+++ src/main/java/dk/adventurealley/app/DAO/DailyIncomeRepository.java
@@ -0,0 +1,25 @@
+package dk.adventurealley.app.DAO;
+
+import dk.adventurealley.app.Model.Entities.Sale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class DailyIncomeRepository {
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    //Læser alle daglige indkomster/omsætning fra sales tabellen
+    public ArrayList<Sale> readAll(){
+        SqlRowSet rs = jdbc.queryForRowSet("SELECT * FROM sales");
+        ArrayList<Sale> sales = new ArrayList<>();
+        while (rs.next()){
+            sales.add(new Sale(rs.getInt("id"),rs.getDate("date").toLocalDate(), rs.getDouble("totalPrice")));
+        }
+        return sales;
+    }
+}
